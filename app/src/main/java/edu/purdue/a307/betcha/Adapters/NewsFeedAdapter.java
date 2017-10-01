36e8abede3ec791d3d024b23cd84eee0539e5738
Peter Jones <peternjones95@gmail.com>
2017-10-01 15:09:35 -0400

    Add bet test model and adapter

--- app/src/main/java/edu/purdue/a307/betcha/Adapters/NewsFeedAdapter.java
+++ app/src/main/java/edu/purdue/a307/betcha/Adapters/NewsFeedAdapter.java
@@ -1,38 +1,44 @@
 package edu.purdue.a307.betcha.Adapters;
 
-import android.content.Context;
-import android.support.v4.widget.TextViewCompat;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import edu.purdue.a307.betcha.Models.PublicFeedItem;
+import edu.purdue.a307.betcha.Models.Bet;
 import edu.purdue.a307.betcha.R;
 
 /**
  * Created by Peter on 9/28/17.
+ *
+ * Adapter for the Public News Feed
  */
 
 public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.ViewHolder> {
-    private String[] dataset;
+    private Bet[] dataset;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mTextTitle;
+        public TextView mBetTitle;
+        public TextView mBetText;
+        public TextView mId;
         public ViewHolder(View v) {
             super(v);
-            mTextTitle = (TextView) v.findViewById(R.id.textTitle);
+            mId = (TextView) v.findViewById(R.id.bet_id);
+            mBetTitle = (TextView) v.findViewById(R.id.bet_title);
+            mBetText = (TextView) v.findViewById(R.id.bet_text);
         }
     }
 
     public NewsFeedAdapter() {
         super();
-        String[] testData = {
-                "bet 1",
-                "bet 2",
-                "bet 3"
-        };
+        // TODO - Add live data
+        Bet[] testData = new Bet[3];
+
+        testData[0] = new Bet(0, 5, "Test1", "I built this for testing", false);
+        testData[1] = new Bet(1, 6, "Test2", "I built this for testing fam", false);
+        testData[2] = new Bet(2, 7, "Test3", "I built this for testing bro", false);
 
         dataset = testData;
     }
@@ -40,15 +46,15 @@ public class NewsFeedAdapter extends RecyclerView.Adapter<NewsFeedAdapter.ViewHo
 
     @Override
     public NewsFeedAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_bet_information, parent, false);
+        CardView v = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_news_feed_bet, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mTextTitle.setText(dataset[position]);
+        holder.mBetTitle.setText(dataset[position]);
     }
 
     @Override
--- app/src/main/java/edu/purdue/a307/betcha/Models/Bet.java
+++ app/src/main/java/edu/purdue/a307/betcha/Models/Bet.java
@@ -9,9 +9,17 @@ package edu.purdue.a307.betcha.Models;
 public class Bet {
 
     public int id;
-    public String maxUsers;
+    public int maxUsers;
     public String title;
     public String text;
     public boolean completed;
 
+
+    public Bet(int id, int maxUsers, String title, String text, boolean completed) {
+        this.id = id;
+        this.maxUsers = maxUsers;
+        this.title = title;
+        this.text = text;
+        this.completed = completed;
+    }
 }
