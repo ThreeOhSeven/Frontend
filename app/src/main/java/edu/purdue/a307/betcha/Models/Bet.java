e4e636a444002f91afd7c3c0a1fb3d7ab116adbe
Peter Jones <peternjones95@gmail.com>
2017-10-01 14:57:19 -0400

    Add test bet model

---
+++ app/src/main/java/edu/purdue/a307/betcha/Models/Bet.java
@@ -0,0 +1,17 @@
+package edu.purdue.a307.betcha.Models;
+
+
+
+/**
+ * Created by Peter on 10/1/17.
+ */
+
+public class Bet {
+
+    public int id;
+    public String maxUsers;
+    public String title;
+    public String text;
+    public boolean completed;
+
+}
